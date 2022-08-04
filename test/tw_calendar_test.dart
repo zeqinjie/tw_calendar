fc55d49e15e1aabe397480bec9e047877afd9b9d
郑泽钦 <zhengzeqin@addcn.com>
2022-08-04 16:55:14 +0800

    <rm>: 非必要文件

--- test/tw_calendar_test.dart
+++ test/tw_calendar_test.dart
@@ -1,29 +1,9 @@
+/*
+ * @Author: zhengzeqin
+ * @Date: 2022-07-29 10:34:05
+ * @LastEditTime: 2022-08-04 16:54:14
+ * @Description: your project
+ */
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tw_calendar/tw_calendar.dart';
-import 'package:tw_calendar/tw_calendar_platform_interface.dart';
-import 'package:tw_calendar/tw_calendar_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockTwCalendarPlatform 
-    with MockPlatformInterfaceMixin
-    implements TwCalendarPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
-
-void main() {
-  final TwCalendarPlatform initialPlatform = TwCalendarPlatform.instance;
-
-  test('$MethodChannelTwCalendar is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelTwCalendar>());
-  });
-
-  test('getPlatformVersion', () async {
-    TwCalendar twCalendarPlugin = TwCalendar();
-    MockTwCalendarPlatform fakePlatform = MockTwCalendarPlatform();
-    TwCalendarPlatform.instance = fakePlatform;
-  
-    expect(await twCalendarPlugin.getPlatformVersion(), '42');
-  });
-}
+void main() {}
